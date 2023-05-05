300d852857d8d1a5e7a9dcbc628f82676b2f7771
Nastya125 <nastyatrue@mail.ru>
2023-05-05 18:55:17 +0300

    Initial

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title_h1'>Ты сегодня покормил кота?</h1>
+    </div>
+  );
+}
+
+export default App;
