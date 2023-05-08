2215767f7c2c0b17d0c2afc2835435a4c59370b6
Nastya125 <nastyatrue@mail.ru>
2023-05-08 20:24:36 +0300

    dinamic content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,12 @@ import Card from "../Card/Card"
 function App() {
   return (
     <div className="App">
-      <h1 className='title_h1'>Ты сегодня покормил кота?</h1>
-      <Card />
+      <h1 className='App__title_h1'>Ты сегодня покормил кота?</h1>
+      <div className="App__container">
+        <Card quantity={10} gift={"мышь в подарок"} weight={"0,5"}/>
+        <Card quantity={40} gift={"2 мыши в подарок"} weight={"2"}/>
+        <Card quantity={100} gift={"5 мышей в подарок заказчик доволен"} weight={"5"}/>
+      </div>
     </div>
   );
 }
